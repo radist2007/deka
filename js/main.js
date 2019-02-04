73dd3b7ebe869a43fa0f1763aeaf896b73cc8c63
Alex <izofatov2007@gmail.com>
2019-02-04 17:40:06 +0200

    add materialize

--- js/main.js
+++ js/main.js
@@ -1,23 +1,5 @@
 console.log("yep");
-function switchMap(obj, value) {
-  console.log('swithcMap(' + value + ');');
-    document.all.div_map.getElementsByTagName('img')[obj.getAttribute('image_id') - 1].style.visibility = value;
-    obj.blur();
-}
-function activateRegion() {
-    var divsArr = document.all.div_map.getElementsByTagName('div');
-    var curRegion = "Киевская область";
-    for (var a = 0, b = divsArr.length; a < b; a++) {
-        var content = divsArr[a].innerHTML.toLowerCase().replace('<br>', ' ');
-        content = content.replace('- ', '-');
-        if ((curRegion == content + ' область' || curRegion == content + ' oblast') || ((content.search('крым') != -1 && curRegion.search('крым') != -1) || (content.search('крим') != -1 && curRegion.search('крим') != -1) || (content.search('crimea') != -1 && curRegion.search('crimea') != -1))) {
-            document.all.div_map.getElementsByTagName('img')[a].style.visibility = 'visible';
-            document.all.regions_map.getElementsByTagName('area')[a].onmouseover = '';
-            document.all.regions_map.getElementsByTagName('area')[a].onmouseout = '';
-            document.all.regions_map.getElementsByTagName('area')[a].removeAttribute('onmouseover');
-            document.all.regions_map.getElementsByTagName('area')[a].removeAttribute('onmouseout');
-            document.all.regions_map.getElementsByTagName('area')[a].removeAttribute('href');
-            return;
-        }
-    }
-}activateRegion();;
+
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
+});
