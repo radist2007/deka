9489acb5a38004a15463ebd09d2ad8a628120dcf
Alex <izofatov2007@gmail.com>
2019-02-06 03:54:08 +0200

    add login&user_room

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 console.log("yep");
 
-$(document).ready(function() {
-  $(".button-collapse").sideNav();
-});
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.fixed-action-btn').floatingActionButton();
+  });
+
+
+
+const username = new MDCTextField(document.querySelector('.username'));
+const password = new MDCTextField(document.querySelector('.password'));
